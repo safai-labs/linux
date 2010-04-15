57b94163f1d2b7852ce033f0cd5b9e4e9f7a0f23
Amir Goldstein <amir@ctera.com>
2010-04-15 12:37:29 +0300

    patch release-1.0.9-rc4.patch

--- fs/next3/snapshot.h
+++ fs/next3/snapshot.h
@@ -20,7 +20,7 @@
 #include "snapshot_debug.h"
 
 
-#define NEXT3_SNAPSHOT_VERSION "next3 snapshot v1.0.9-rc3"
+#define NEXT3_SNAPSHOT_VERSION "next3 snapshot v1.0.9-rc4"
 
 /*
  * use signed 64bit for snapshot image addresses

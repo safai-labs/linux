37bb87c37b56bfd9fbb111550012c3af246705b5
Amir Goldstein <amir73il@users.sf.net>
2010-07-20 10:22:24 +0300

    patch fix_nested_ifdef.patch

--- fs/next3/next3_jbd.h
+++ fs/next3/next3_jbd.h
@@ -174,9 +174,7 @@ struct next3_handle_s
 	unsigned int	h_sync:		1;	/* sync-on-close */
 	unsigned int	h_jdata:	1;	/* force data journaling */
 	unsigned int	h_aborted:	1;	/* fatal error on handle */
-#ifdef CONFIG_NEXT3_FS_SNAPSHOT_BLOCK
 	unsigned int	h_cowing:	1;	/* COWing block to snapshot */
-#endif
 #ifdef CONFIG_NEXT3_FS_SNAPSHOT_JOURNAL_CREDITS
 	/* Number of buffers requested by user:
 	 * (before adding the COW credits factor) */

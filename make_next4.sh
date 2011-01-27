d4e7c4a30715214d9266ec4cd74c7d01aa7eb981
Amir Goldstein <amir73il@users.sf.net>
2011-01-27 12:32:44 +0200

    remove unsupported tests from next4 script
    Ext4 snapshots data is not consistent when files are written in-place.
    Remove the tests that write in-place from next4 tests script.
    
    Also make next4 script work with both next4 and ext4 modules.
    The script uses the modoule name as the name of the script itself.

--- make_next4.sh
+++ make_next4.sh
@@ -4,5 +4,6 @@ KERNEL=$(uname -r)
 KDIR=/lib/modules/${KERNEL}/build
 make -C ${KDIR} M=${PWD}/fs/next4 modules
 sudo make -C ${KDIR} M=${PWD}/fs/next4 modules_install
+sudo rmmod next4
 sudo modprobe next4
 

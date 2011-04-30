d9385f39cc099abe2d0a270cbb2afdba17ae5b4d
Yongqiang Yang <xiaoqiangnk@gmail.com>
2011-04-30 05:54:56 -0700

    Remove submit-snapshot_patches.sh

--- submit_snapshot_patches.sh
+++
@@ -1,8 +0,0 @@
-BASE=ext4-next
-PATCH=ext4_snapshots.patch
-BRANCH=ext4-snapshot-patches
-
-# re-create the ext4-snapshot-patches branch from ext4-next branch
-(git branch | grep $BRANCH) && (git branch -D $BRANCH || exit 1)
-git checkout -b $BRANCH $BASE|| exit 1
-

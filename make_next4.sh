8c1a146a6b93dee114f0d4ae89a72842dda3dda0
Amir Goldstein <amir73il@users.sf.net>
2011-05-11 15:48:07 +0300

    install next4 trace events header

--- make_next4.sh
+++ make_next4.sh
@@ -2,6 +2,8 @@
 
 KERNEL=$(uname -r)
 KDIR=/lib/modules/${KERNEL}/build
+
+sudo cp include/trace/events/next4.h ${KDIR}/include/trace/events/
 make -C ${KDIR} M=${PWD}/fs/next4 modules
 sudo make -C ${KDIR} M=${PWD}/fs/next4 modules_install
 sudo rmmod next4

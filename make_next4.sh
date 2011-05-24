5f52861c659c868c2a6ccb97e8c88929dfe78dbf
Amir Goldstein <amir73il@users.sf.net>
2011-05-24 21:48:49 +0300

    clone include/trace/events/next4.h for standalone build

--- make_next4.sh
+++ make_next4.sh
@@ -3,7 +3,7 @@
 KERNEL=$(uname -r)
 KDIR=/lib/modules/${KERNEL}/build
 
-sudo cp include/trace/events/next4.h ${KDIR}/include/trace/events/
+sudo cp fs/next4/next4_events.h ${KDIR}/include/trace/events/next4.h
 make -C ${KDIR} M=${PWD}/fs/next4 modules
 sudo make -C ${KDIR} M=${PWD}/fs/next4 modules_install
 sudo rmmod next4

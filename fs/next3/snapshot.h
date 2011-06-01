87a6639f2f440685d5a550b0ce759ad39e511e58
Amir Goldstein <amir73il@users.sf.net>
2011-06-01 14:04:53 +0300

    enforce s_bitmap_maxbytes in next3

--- fs/next3/snapshot.h
+++ fs/next3/snapshot.h
@@ -21,7 +21,7 @@
 #include "snapshot_debug.h"
 
 
-#define NEXT3_SNAPSHOT_VERSION "next3 snapshot v1.0.13-6 (2-May-2010)"
+#define NEXT3_SNAPSHOT_VERSION "next3 snapshot v1.0.13-7 (1-Jun-2010)"
 
 /*
  * use signed 64bit for snapshot image addresses

d814106cf4cef17cf2d49c15d0759d203c2af5fe
Yongqiang Yang <xiaoqiangnk@gmail.com>
2011-06-08 16:02:11 +0800

    EXT4_MAP_REMAP is cleared in ext4_map_blocks()

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -3385,7 +3385,6 @@ found:
 	if (!(flags & EXT4_GET_BLOCKS_CREATE))
 		goto out;
 
-	map->m_flags &= ~EXT4_MAP_REMAP;
 	if ((path == NULL) && (flags & EXT4_GET_BLOCKS_CREATE)) {
 		/* find extent for this block */
 		path = ext4_ext_find_extent(inode, map->m_lblk, NULL);

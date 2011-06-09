b5fa57fa66d447dc0a47276d2bb5d1792e2cab2d
Yongqiang Yang <xiaoqiangnk@gmail.com>
2011-06-09 11:33:07 +0800

    Remove duplicate condition state

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -3382,7 +3382,7 @@ found:
 	if (!(flags & EXT4_GET_BLOCKS_CREATE))
 		goto out;
 
-	if ((path == NULL) && (flags & EXT4_GET_BLOCKS_CREATE)) {
+	if (path == NULL) {
 		/* find extent for this block */
 		path = ext4_ext_find_extent(inode, map->m_lblk, NULL);
 		if (IS_ERR(path)) {

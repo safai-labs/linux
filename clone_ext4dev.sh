1713b58e6362d1af85a1e17f892bbe79a7ca023c
Amir Goldstein <amir73il@users.sf.net>
2011-06-15 09:06:43 +0300

    add ext4dev to fs/Makefile
    ext4dev/Kconfig is not included in fs/Kconfig on purpose.
    Configurations options are defined in ext4dev.h.
    clone_ext4dev.sh prepares a patch for adding ext4dev fs to kernel build.

--- clone_ext4dev.sh
+++ clone_ext4dev.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# re-create the clone_ext4dev branch from current branch
+(git branch | grep clone_ext4dev) && (git branch -D clone_ext4dev || exit 1)
+git checkout -b clone_ext4dev
+
 rm -rf fs/ext4dev
 mkdir -p fs/ext4dev
 cp -a fs/ext4/*.h fs/ext4dev
@@ -16,3 +20,9 @@ mv ext4.h ext4dev.h
 sed -f ../../ext4dev.sed -i *
 cd ..
 tar cfz ../ext4dev_module.tar.gz ext4dev/
+cd ..
+cp -a fs/ext4dev/ext4dev_events.h include/trace/events/ext4dev.h
+
+git add fs/ext4dev include/trace/events/ext4dev.h
+git commit -m "add ext4dev fs"
+git diff v2.6.38 fs/Makefile fs/ext4dev include/trace/events/ext4dev.h > ext4dev_fs.patch

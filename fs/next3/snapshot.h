fd422413ce4675e435d58c09fd8232ddc52af836
Amir Goldstein <amir73il@users.sf.net>
2011-06-16 11:37:26 +0300

    fix year in snapshot version string

--- fs/next3/snapshot.h
+++ fs/next3/snapshot.h
@@ -21,7 +21,7 @@
 #include "snapshot_debug.h"
 
 
-#define NEXT3_SNAPSHOT_VERSION "next3 snapshot v1.0.13-7 (15-Jun-2010)"
+#define NEXT3_SNAPSHOT_VERSION "next3 snapshot v1.0.13-7 (15-Jun-2011)"
 
 /*
  * use signed 64bit for snapshot image addresses

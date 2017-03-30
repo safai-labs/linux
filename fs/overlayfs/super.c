5e25086412453fc74ea1896c048c18d0b16e3701
Amir Goldstein <amir73il@gmail.com>
2017-03-30 15:44:03 +0300

    ovl: explicit copy up/whiteout requests from snapshot mount
    snapshot mount allows changes to overlayfs lower, while maintaining
    a stable view in merge of upper+lower.
    
    snapshot mount calls the underlying overlayfs d_real() with
    O_RDWR open_flags.
    Unlike vfs_open(), snapshot mount can pass non zero open_flags
    with non regular dentry as well as with a negative dentry.
    
    d_real() is never called from vfs with non zero open_flags and
    non regular/negative dentry, so this change has no effect on existing
    overlayfs vfs operations.
    
    The purpose of the call with non regular dentry is a request for copy
    up of dir/symlink in preparation of upcoming delete or modify in lower.
    
    The purpose of the call with negative dentry is a request for explicit
    whiteout, in preparation for upcoming create in lower.
    
    ovl_do_remove() is altered to perform explicit whiteout when called
    a negative dentry.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -71,30 +71,57 @@ static int ovl_check_append_only(struct inode *inode, int flag)
 	return 0;
 }
 
+/*
+ * snapshot mount calls the underlying overlayfs d_real() with
+ * O_RDWR open_flags.
+ * Unlike vfs_open(), snapshot mount can pass non regular dentry
+ * with non zero open_flags as well as negative dentry with open_flags.
+ * The purpose of the call with non regular dentry is a request for copy
+ * up of dir/symlink in preparation of upcoming delete or modify in lower.
+ * The purpose of the call with negative dentry is a request for explicit
+ * whiteout, in preparation for upcoming create in lower.
+ */
 static struct dentry *ovl_d_real(struct dentry *dentry,
 				 const struct inode *inode,
 				 unsigned int open_flags)
 {
 	struct dentry *real;
-	bool rocopyup = !inode && ovl_consistent_fd(dentry->d_sb);
+	bool rocopyup = !inode && d_is_reg(dentry) &&
+			ovl_consistent_fd(dentry->d_sb);
 	int err;
 
 	if (WARN_ON(open_flags && inode))
 		return dentry;
 
-	if (!d_is_reg(dentry)) {
-		if (!inode || inode == d_inode(dentry))
+	/* Negative dentry with non zero open_flags means explicit whiteout */
+	if (d_is_negative(dentry)) {
+		if (!open_flags)
 			return dentry;
-		goto bug;
+
+		/* Remove with negative dentry means explicit whiteout */
+		if (!OVL_TYPE_OPAQUE(ovl_path_type(dentry))) {
+			err = ovl_do_remove(dentry, 0);
+			if (err)
+				return ERR_PTR(err);
+		}
+
+		return dentry;
 	}
 
-	if (d_is_negative(dentry))
-		return dentry;
-
+	/* Non regular file with non zero open_flags means explicit copy up */
 	if (open_flags || rocopyup) {
 		err = ovl_open_maybe_copy_up(dentry, open_flags, rocopyup);
 		if (err)
 			return ERR_PTR(err);
+
+		if (!d_is_reg(dentry))
+			return dentry;
+	}
+
+	if (!d_is_reg(dentry)) {
+		if (!inode || inode == d_inode(dentry))
+			return dentry;
+		goto bug;
 	}
 
 	real = ovl_dentry_upper(dentry);

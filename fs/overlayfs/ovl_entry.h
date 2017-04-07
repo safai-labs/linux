19522c60720ffe2bf73762ca14653b6586def1af
Amir Goldstein <amir73il@gmail.com>
2017-04-07 14:22:02 +0300

    ovl: copy on read with consistent_fd feature
    Overlayfs copies up a file when the file is opened for write.  Writes
    to the returned file descriptor are not visible to file descriptors
    that were opened for read prior to copy up.
    
    If this config option is enabled then overlay filesystems will default
    to copy up a file that is opened for read to avoid this inconsistency.
    In this case, it is still possible to turn off consistent fd globally
    with the "consistent_fd=off" module option or on a filesystem instance
    basis with the "consistent_fd=off" mount option.
    
    The feature will not be turned on for an overlay mount unless all
    the layers are on the same underlying filesystem and this filesystem
    supports clone.
    
    This introduces a slight change in d_real() semantics. Now d_real()
    may return an error with NULL inode argument also in the zero open
    flags case. vfs API documentation has been updated.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/overlayfs/ovl_entry.h
+++ fs/overlayfs/ovl_entry.h
@@ -14,6 +14,7 @@ struct ovl_config {
 	char *workdir;
 	bool default_permissions;
 	bool redirect_dir;
+	bool consistent_fd;
 };
 
 /* private information held for overlayfs's superblock */
@@ -30,6 +31,7 @@ struct ovl_fs {
 	bool tmpfile;
 	bool noxattr;
 	bool cloneup;
+	bool rocopyup;	/* copy up on open for read */
 	wait_queue_head_t copyup_wq;
 	/* sb common to all layers */
 	struct super_block *same_sb;

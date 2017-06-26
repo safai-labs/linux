4c995d05056afe85f1ff8f8436fc7c46cf15eeb8
Amir Goldstein <amir73il@gmail.com>
2017-06-26 13:45:27 +0300

    ovl: cow file/dir to snapshot on ovl_want_write()
    In a snapshot mount, any operation that wants to modify upper will
    trigger cow to snapshot.
    
    Created ovl_snapshot_{want,drop}_write helpers.
    
    Code should call ovl_snapshot_want_write() before create/delete/modify
    an entry and ovl_snapshot_drop_write() when done.
    Currently, ovl_snapshot_want_write() triggers copy down to snapshot and
    ovl_snapshot_drop_write() does nothing.
    
    Most operations already call ovl_{want,drop}_write() before performing
    create/delete/modify, so hook snapshot cow to these helpers and add
    explicit call to snapshot cow for destination dentry of rename/link.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/overlayfs/dir.c
+++ fs/overlayfs/dir.c
@@ -613,9 +613,17 @@ static int ovl_link(struct dentry *old, struct inode *newdir,
 	bool locked = false;
 	struct inode *inode;
 
-	err = ovl_want_write(old);
+	/*
+	 * Keep before ovl_want_write() because cow to snapshot
+	 * aquires and releases sb_writers of upper/lower
+	 */
+	err = ovl_snapshot_want_write(new);
 	if (err)
 		goto out;
+
+	err = ovl_want_write(old);
+	if (err)
+		goto out_snapshot_drop_write;
 
 	err = ovl_copy_up(old);
 	if (err)
@@ -635,6 +643,8 @@ static int ovl_link(struct dentry *old, struct inode *newdir,
 	ovl_nlink_end(old, locked);
 out_drop_write:
 	ovl_drop_write(old);
+out_snapshot_drop_write:
+	ovl_snapshot_drop_write(new);
 out:
 	return err;
 }
@@ -949,9 +959,17 @@ static int ovl_rename(struct inode *olddir, struct dentry *old,
 	if (!overwrite && !ovl_can_move(new))
 		goto out;
 
-	err = ovl_want_write(old);
+	/*
+	 * Keep before ovl_want_write() because cow to snapshot
+	 * aquires and releases sb_writers of upper/lower
+	 */
+	err = ovl_snapshot_want_write(new);
 	if (err)
 		goto out;
+
+	err = ovl_want_write(old);
+	if (err)
+		goto out_snapshot_drop_write;
 
 	err = ovl_copy_up(old);
 	if (err)
@@ -1110,6 +1128,8 @@ out_revert_creds:
 	ovl_nlink_end(new, locked);
 out_drop_write:
 	ovl_drop_write(old);
+out_snapshot_drop_write:
+	ovl_snapshot_drop_write(new);
 out:
 	dput(opaquedir);
 	return err;

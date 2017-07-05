0263eefaf24b95fdda8fb9e6c5627c62fac15b86
Amir Goldstein <amir73il@gmail.com>
2017-07-05 22:06:29 +0300

    ovl: prepare for changing snapshot overlay
    Drop the assumption that ofs->snapshot_mnt and the snapshot overlay
    dentries oe->__snapdentry are not changing throughout the life time
    of the snapshot mount:
    - Rename the ovl_fs struct field to __snapmnt
    - Access __snapmnt and __snapdentry using rcu_dereference()
    - Use helpers ovl_snapshot_path() and ovl_snapshot_dentry()
      to get __snapdentry and __snapmnt with elevated refcount
    - Never access those pointers for non snapshot mount case
    
    ovl_snapshot_path() returns error for the following cases:
    ESTALE - __snapdentry is not from __snapmnt
    ENOENT - __snapdentry is NULL and __snapmnt is not NULL
    
    ESTALE implies that __snapdentry should be looked up again after
    __snapmnt has changed.
    
    ENOENT implies that __snapdentry should be looked up after
    __snapmnt was set for the first time.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -225,7 +225,7 @@ static void ovl_put_super(struct super_block *sb)
 	if (ufs->upper_mnt)
 		ovl_inuse_unlock(ufs->upper_mnt->mnt_root);
 	mntput(ufs->upper_mnt);
-	mntput(ufs->snapshot_mnt);
+	mntput(ufs->__snapmnt);
 	for (i = 0; i < ufs->numlower; i++) {
 		mntput(ufs->lower_mnt[i].mnt);
 		free_anon_bdev(ufs->lower_mnt[i].pseudo_dev);
@@ -1055,7 +1055,7 @@ static int ovl_fill_super(struct super_block *sb, void *data, int silent)
 		if (IS_ERR(snapmnt))
 			goto out_put_upper_mnt;
 
-		ufs->snapshot_mnt = snapmnt;
+		ufs->__snapmnt = snapmnt;
 	}
 
 	if (ufs->config.workdir) {
@@ -1296,7 +1296,7 @@ out_put_workdir:
 	dput(ufs->workdir);
 out_put_upper_mnt:
 	mntput(ufs->upper_mnt);
-	mntput(ufs->snapshot_mnt);
+	mntput(ufs->__snapmnt);
 out_put_lowerpath:
 	for (i = 0; i < numlower; i++)
 		path_put(&stack[i]);
@@ -1313,6 +1313,7 @@ out_put_upperpath:
 	path_put(&upperpath);
 	path_put(&snappath);
 out_free_config:
+	kfree(ufs->config.snapshot);
 	kfree(ufs->config.lowerdir);
 	kfree(ufs->config.upperdir);
 	kfree(ufs->config.workdir);
